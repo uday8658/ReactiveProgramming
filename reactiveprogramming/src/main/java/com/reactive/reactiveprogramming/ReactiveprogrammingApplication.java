6212e2d886016be95d5ce1dcf4148e236a84967c
uday <udaysingh8658@gmail.com>
2020-02-04 10:18:00 +0530

    added
    say hi

--- reactiveprogramming/src/main/java/com/reactive/reactiveprogramming/ReactiveprogrammingApplication.java
+++ reactiveprogramming/src/main/java/com/reactive/reactiveprogramming/ReactiveprogrammingApplication.java
@@ -8,6 +8,7 @@ public class ReactiveprogrammingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveprogrammingApplication.class, args);
+		System.out.println("hi");
 	}
 
 }
